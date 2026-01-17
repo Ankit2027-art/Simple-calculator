5531c69c45bdb9cdb28448349a49a4f42b413c88
Ankit Patel <ankit8400764325@gmail.com>
2026-01-17 12:41:49 +0530

    code

--- Main.py
+++ Main.py
@@ -1,2 +1,27 @@
 # Simple-calculator
-A simple Python calculator that performs basic arithmetic operations using match-case and handles invalid input with exception handling.
+# A simple Python calculator that performs basic arithmetic operations using match-case and handles invalid input with exception handling
+
+try:
+    a = int(input("Enter a First number:"))
+
+    b = int(input("Enter a second number:"))
+
+    print("What kind of opration can you Perform.\n Press + for Addition \n press - for Subtraction \n press * for Multiplication \n press / for division")
+
+    o = input("Enter Opration:")
+
+    match o:
+        case "+":
+            print(f"The result is:{a+b}")
+        case "-":
+            print(f"The result is:{a-b}")
+        case "*":
+            print(f"The result is:{a*b}")
+        case "/":
+            print(f"The result is:{a/b}")
+        case _:
+            print("Enter a valid Opration")
+
+
+except Exception as e:
+    print("Enter a valid value of a and b")
